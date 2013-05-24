651da475f36f656574a70f9f21ef08081f2c0b1d
daggs <melhuishrob@gmail.com>
2013-05-24 01:32:33 +0100

    v1.1
    - hand-place-able  half doors!
    - no physics for user-defined blocks
    (ladders, rails, torches, dragon eggs, half-doors)
    - /get <argument> command to give user a variety of blocks:
    (dragon eggs, double-slabes [0-15], half-doors, 'special-logs', lit
    furnaces)

--- src/main/com/dags/BuildFixes/BuildFixes.java
+++ src/main/com/dags/BuildFixes/BuildFixes.java
@@ -1,5 +1,8 @@
 package com.dags.BuildFixes;
 
+import java.util.HashSet;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -8,9 +11,13 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BuildFixes extends JavaPlugin {
-	
+
+	@SuppressWarnings("rawtypes")
+	public static HashSet noPhysList = new HashSet();
+	public static boolean doors;
 	public static boolean logs;
 	public static boolean lamps;
+	public static boolean noPhysics;
 	public static boolean eggBreak;
 	
 	
@@ -26,14 +33,25 @@ public class BuildFixes extends JavaPlugin {
     
     public void setupConfig() {
         getConfig().options().copyDefaults(true);
+        setupNoPhysList();
         saveConfig();
     }
     
     public void registerEvents() {
+    	doors = getConfig().getBoolean("Settings.HalfDoors");
     	logs = getConfig().getBoolean("Settings.SpecialLogs");
     	lamps = getConfig().getBoolean("Settings.PlaceLamps");
+    	noPhysics = getConfig().getBoolean("Settings.NoPhysics");
     	eggBreak = getConfig().getBoolean("Settings.DragonEggBlocking");
     	this.getServer().getPluginManager().registerEvents(new EventListener(), this);
+    }
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void setupNoPhysList() {
+        List NPlist = getConfig().getList("NoPhysicsList");
+        for (Object o : NPlist) {
+        	noPhysList.add(o);
+        }
     }
     
   //GIVE SPECIAL LOGS
@@ -45,18 +63,77 @@ public class BuildFixes extends JavaPlugin {
 		}
 		else{
 			Player player = (Player) sender;
-			if(label.equalsIgnoreCase("logs")
-					&& sender.hasPermission("BuildFixes.logs")){
-				if(logs){
-					player.getInventory().addItem( new ItemStack(17, 64, (byte) 12 ));
-					player.getInventory().addItem(new ItemStack(17, 64, (byte) 13));
-					player.getInventory().addItem(new ItemStack(17, 64, (byte) 14));
-					player.getInventory().addItem(new ItemStack(17, 64, (byte) 15));
+			if(label.equalsIgnoreCase("get")
+					&& sender.hasPermission("BuildFixes.get")
+					){
+				if(args.length == 0){
+					player.sendMessage(ChatColor.GRAY + "/get [doors], [egg], [furnace], [logs], [slabs #]");
 					return true;
 				}
-				else{
-					sender.sendMessage(ChatColor.GRAY + "Feature disabled!");
+				if(args[0].equalsIgnoreCase("egg")&& sender.hasPermission("BuildFixes.eggs")){
+					player.getInventory().addItem(new ItemStack(122, 1, (byte) 0));
 					return true;
+				}
+				if((args[0].equalsIgnoreCase("doors")
+						|| args[0].equalsIgnoreCase("door"))
+						&& sender.hasPermission("BuildFixes.doors")){
+					player.getInventory().addItem(new ItemStack(64, 64, (byte) 0));
+					player.getInventory().addItem(new ItemStack(71, 64, (byte) 0));
+					return true;
+				}
+				if(args[0].equalsIgnoreCase("furnace")
+						&& sender.hasPermission("BuildFixes.furnace")){
+					player.getInventory().addItem(new ItemStack(62, 64, (byte) 0));
+					return true;
+				}
+				if((args[0].equalsIgnoreCase("logs")
+						|| args[0].equalsIgnoreCase("log"))
+						&& sender.hasPermission("BuildFixes.logs")){
+					if(logs){
+						player.getInventory().addItem(new ItemStack(17, 64, (byte) 12 ));
+						player.getInventory().addItem(new ItemStack(17, 64, (byte) 13));
+						player.getInventory().addItem(new ItemStack(17, 64, (byte) 14));
+						player.getInventory().addItem(new ItemStack(17, 64, (byte) 15));
+						return true;
+					}
+					else{
+						sender.sendMessage(ChatColor.GRAY + "Feature disabled!");
+						return true;
+					}
+				}
+				if((args[0].equalsIgnoreCase("slabs")
+						|| args[0].equalsIgnoreCase("slab"))
+						&& sender.hasPermission("BuildFixes.slabs")){
+					if(args.length > 1){
+		    			String str = args[1];
+		    			if (str == null) {
+		    				return false;
+		    			}
+		    			int length = str.length();
+		    			if (length == 0) {
+		    				return false;
+		    			}
+		    			int i = 0;
+		    			if (str.charAt(0) == '-') {
+		    				if (length == 1) {
+		    					return false;
+		    				}
+		    				i = 1;
+		    			}
+		    			for (; i < length; i++) {
+		    				char c = str.charAt(i);
+		    				if (c <= '/' || c >= ':') {
+		    					return false;
+		    				}
+		    			}
+						int dmg = Integer.valueOf(args[1]);
+						player.getInventory().addItem(new ItemStack(43, 64, (byte) dmg));
+						return true;
+					}
+					else{
+						player.sendMessage(ChatColor.GRAY + "/get slabs [0-16]");
+						return true;
+					}
 				}
 			}
 		//ADMIN COMMANDS
@@ -83,6 +160,22 @@ public class BuildFixes extends JavaPlugin {
 			    			return true;
 			    		}
 					}
+					if(args[0].equalsIgnoreCase("doors")){
+						if(doors){
+			    			this.getConfig().set("Settings.HalfDoors", false);
+			    			this.saveConfig();
+			    			doors = false;
+			    			sender.sendMessage(ChatColor.RED + "Disabled!");
+			    			return true;
+			    		}
+			    		else{
+			    			this.getConfig().set("Settings.HalfDoors", true);
+			    			this.saveConfig();
+			    			doors = true;
+			    			sender.sendMessage(ChatColor.GREEN + "Enabled!");
+			    			return true;
+			    		}
+					}
 					if(args[0].equalsIgnoreCase("lamps")){
 						if(lamps){
 			    			this.getConfig().set("Settings.PlaceLamps", false);
@@ -95,6 +188,22 @@ public class BuildFixes extends JavaPlugin {
 			    			this.getConfig().set("Settings.PlaceLamps", true);
 			    			this.saveConfig();
 			    			lamps = true;
+			    			sender.sendMessage(ChatColor.GREEN + "Enabled!");
+			    			return true;
+			    		}
+					}
+					if(args[0].equalsIgnoreCase("nophysics")){
+						if(noPhysics){
+			    			this.getConfig().set("Settings.NoPhysics", false);
+			    			this.saveConfig();
+			    			noPhysics = false;
+			    			sender.sendMessage(ChatColor.RED + "Disabled!");
+			    			return true;
+			    		}
+			    		else{
+			    			this.getConfig().set("Settings.NoPhysics", true);
+			    			this.saveConfig();
+			    			noPhysics = true;
 			    			sender.sendMessage(ChatColor.GREEN + "Enabled!");
 			    			return true;
 			    		}
--- src/main/com/dags/BuildFixes/EventListener.java
+++ src/main/com/dags/BuildFixes/EventListener.java
@@ -1,15 +1,20 @@
 package com.dags.BuildFixes;
 
+import static com.dags.BuildFixes.BuildFixes.doors;
 import static com.dags.BuildFixes.BuildFixes.eggBreak;
 import static com.dags.BuildFixes.BuildFixes.lamps;
 import static com.dags.BuildFixes.BuildFixes.logs;
+import static com.dags.BuildFixes.BuildFixes.noPhysics;
+import static com.dags.BuildFixes.BuildFixes.noPhysList;
 
 import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
@@ -17,13 +22,11 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventListener implements Listener{
 	
-	static int logData;
-	
 	@EventHandler(priority = EventPriority.HIGH)
 	private void playerInteract(PlayerInteractEvent event) {
 		//DRAGON EGG INTERACTION BLOCKER!
 		if(eggBreak){
-			if(!(event.getPlayer().hasPermission("BuildFixes.dragonegg"))){
+			if(!(event.getPlayer().hasPermission("BuildFixes.eggbreak"))){
 				if(  (event.getAction().equals(Action.LEFT_CLICK_BLOCK) 
 					|| event.getAction().equals(Action.RIGHT_CLICK_BLOCK))
 					&& event.getClickedBlock().getType().equals(Material.DRAGON_EGG)  ){
@@ -34,25 +37,75 @@ public class EventListener implements Listener{
 	}
 	
     @EventHandler(priority = EventPriority.HIGH)
-    private void blockPhysics(BlockPhysicsEvent event) { 
-    	if(event.getBlock().getType().equals(Material.DRAGON_EGG)){
-    		event.setCancelled(true);
-    	}
-    }
-	
-    @EventHandler(priority = EventPriority.HIGH)
     private void blockPlace(BlockPlaceEvent event) {   
     	Player player = event.getPlayer();
     	//PLACE COVERED LOGS!
     	if(logs){
     		if(player.hasPermission("BuildFixes.logs")
     			&& player.getItemInHand().getType().equals(Material.LOG)){
-    			logData = player.getItemInHand().getData().getData();
+    			int logData = player.getItemInHand().getData().getData();
     			if(logData >= 12){
     				event.getBlock().setData((byte) logData);
     			}
     		}
     	}
+    	//PLACE HALF DOORS!
+    	if(player.hasPermission("BuildFixes.doors") && doors){
+    		int blockType = player.getItemInHand().getTypeId();
+    		int yaw = (int) player.getLocation().getYaw();
+    		if(blockType == 64 || blockType == 71){
+    			//NORTH
+    			if(  yaw > -225
+    					&& yaw < -135){
+    				int dirData = 3;
+    				event.getBlockPlaced().setTypeId(blockType);
+    				event.getBlockPlaced().setTypeIdAndData(blockType, (byte) dirData, false);
+    				event.getBlockPlaced().getRelative(BlockFace.UP).setTypeIdAndData(blockType, (byte) dirData, false);
+    			}
+    			//EAST
+    			if(yaw >= -135
+    					&& yaw < -45){
+    				int dirData = 0;
+    				event.getBlockPlaced().setTypeId(blockType);
+    				event.getBlockPlaced().setTypeIdAndData(blockType, (byte) dirData, false);
+    				event.getBlockPlaced().getRelative(BlockFace.UP).setTypeIdAndData(blockType, (byte) dirData, false);
+    			}
+    			//SOUTH
+    			if((yaw > -45
+    					&& yaw < 0)
+    					||(yaw > -360
+    							&& yaw < -315)){
+    				int dirData = 1;
+    				event.getBlockPlaced().setTypeId(blockType);
+    				event.getBlockPlaced().setTypeIdAndData(blockType, (byte) dirData, false);
+    				event.getBlockPlaced().getRelative(BlockFace.UP).setTypeIdAndData(blockType, (byte) dirData, false);
+    			}
+    			//WEST
+    			if(yaw > -315
+    					&& yaw < -225){
+    				int dirData = 2;
+    				event.getBlockPlaced().setTypeId(blockType);
+    				event.getBlockPlaced().setTypeIdAndData(blockType, (byte) dirData, false);
+    				event.getBlockPlaced().getRelative(BlockFace.UP).setTypeIdAndData(blockType, (byte) dirData, false);
+    			}
+    		}
+    	}
+    }
+    
+    //BREAK EVENT FOR HALF-DOORS (ALSO REMOVES INVISIBLE HALF)
+    @EventHandler(priority = EventPriority.HIGH)
+    private void blockBreak(BlockBreakEvent event) {
+    	int doorType = event.getBlock().getTypeId();
+    	if(doorType == 64 || doorType == 71){
+    		int blockAbove = event.getBlock().getRelative(BlockFace.UP).getTypeId();
+    		int blockBelow = event.getBlock().getRelative(BlockFace.DOWN).getTypeId();
+    		if(blockAbove == 64 || blockAbove == 71){
+    			event.getBlock().getRelative(BlockFace.UP).setTypeId(0);
+    		}
+    		if(blockBelow == 64 || blockBelow ==71){
+    			event.getBlock().getRelative(BlockFace.DOWN).setTypeId(0);
+    		}
+    	} 
     }
     
     //PLACE POWERED RESTONE LAMPS
@@ -60,6 +113,17 @@ public class EventListener implements Listener{
     private void lampPlace(BlockRedstoneEvent event) {
     	if(lamps && event.getBlock().getType().equals(Material.REDSTONE_LAMP_ON)){
     		event.setNewCurrent(16);
+    	}
+    }
+    
+    //NOPHYSICS HANDLER FOR DRAGON EGGS AND HALF-DOORS
+    @EventHandler(priority = EventPriority.HIGH)
+    private void doorPlace(BlockPhysicsEvent event) {
+    	if(noPhysics){
+    		int npBlockType = event.getBlock().getTypeId();
+    		if(noPhysList.contains(npBlockType)){
+    			event.setCancelled(true);
+    		}
     	}
     }
 }
