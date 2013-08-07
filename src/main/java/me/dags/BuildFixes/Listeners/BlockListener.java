787673f61ccaf53b2789420d68c05ef783c2c04d
daggs <melhuishrob@gmail.com>
2013-08-07 15:07:04 +0100

    Check if place/break event has been cancelled by another plugin before continuing.

--- src/main/java/me/dags/BuildFixes/Listeners/BlockListener.java
+++ src/main/java/me/dags/BuildFixes/Listeners/BlockListener.java
@@ -48,68 +48,75 @@ public class BlockListener implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.NORMAL)
+	@EventHandler(priority = EventPriority.HIGH)
 	private void blockPlace(BlockPlaceEvent event) {
-		Player player = event.getPlayer();
-		if (logs) {
-			if (player.hasPermission("BuildFixes.logs")
-					&& player.getItemInHand().getType().equals(Material.LOG)) {
-				int logData = player.getItemInHand().getData().getData();
-				if (logData >= 12) {
-					event.getBlock().setData((byte) logData);
+		if(!event.isCancelled()){
+			Player player = event.getPlayer();
+			if (logs) {
+				if (player.hasPermission("BuildFixes.logs")
+						&& player.getItemInHand().getType().equals(Material.LOG)) {
+					int logData = player.getItemInHand().getData().getData();
+					if (logData >= 12) {
+						event.getBlock().setData((byte) logData);
+					}
 				}
 			}
-		}
-		if (player.hasPermission("BuildFixes.doors") && doors) {
-			int blockType = player.getItemInHand().getTypeId();
-			int yaw = (int) player.getLocation().getYaw();
-			if (blockType == 64 || blockType == 71) {
-				if ((yaw >= -225 && yaw < -135) || (yaw >= 135 && yaw <= 225)) {
-					event.getBlockPlaced().setTypeId(blockType);
-					event.getBlockPlaced().setTypeIdAndData(blockType,
-							(byte) 3, false);
-					event.getBlockPlaced().getRelative(BlockFace.UP)
-							.setTypeIdAndData(blockType, (byte) 3, false);
-				} else if ((yaw >= -135 && yaw < -45)
-						|| (yaw >= 225 && yaw < 315)) {
-					event.getBlockPlaced().setTypeId(blockType);
-					event.getBlockPlaced().setTypeIdAndData(blockType,
-							(byte) 0, false);
-					event.getBlockPlaced().getRelative(BlockFace.UP)
-							.setTypeIdAndData(blockType, (byte) 0, false);
-				} else if ((yaw >= -45 && yaw < 45)
-						|| (yaw >= -360 && yaw < -315)
-						|| (yaw >= 315 && yaw <= 360)) {
-					event.getBlockPlaced().setTypeId(blockType);
-					event.getBlockPlaced().setTypeIdAndData(blockType,
-							(byte) 1, false);
-					event.getBlockPlaced().getRelative(BlockFace.UP)
-							.setTypeIdAndData(blockType, (byte) 1, false);
-				} else if ((yaw >= -315 && yaw < -225)
-						|| (yaw >= 45 && yaw < 135)) {
-					event.getBlockPlaced().setTypeId(blockType);
-					event.getBlockPlaced().setTypeIdAndData(blockType,
-							(byte) 2, false);
-					event.getBlockPlaced().getRelative(BlockFace.UP)
-							.setTypeIdAndData(blockType, (byte) 2, false);
+			if (player.hasPermission("BuildFixes.doors") && doors) {
+				int blockType = player.getItemInHand().getTypeId();
+				int yaw = (int) player.getLocation().getYaw();
+				if (blockType == 64 || blockType == 71) {
+					if ((yaw >= -225 && yaw < -135) || (yaw >= 135 && yaw <= 225)) {
+						event.getBlockPlaced().setTypeId(blockType);
+						event.getBlockPlaced().setTypeIdAndData(blockType,
+								(byte) 3, false);
+						event.getBlockPlaced().getRelative(BlockFace.UP)
+								.setTypeIdAndData(blockType, (byte) 3, false);
+					} else if ((yaw >= -135 && yaw < -45)
+							|| (yaw >= 225 && yaw < 315)) {
+						event.getBlockPlaced().setTypeId(blockType);
+						event.getBlockPlaced().setTypeIdAndData(blockType,
+								(byte) 0, false);
+						event.getBlockPlaced().getRelative(BlockFace.UP)
+								.setTypeIdAndData(blockType, (byte) 0, false);
+					} else if ((yaw >= -45 && yaw < 45)
+							|| (yaw >= -360 && yaw < -315)
+							|| (yaw >= 315 && yaw <= 360)) {
+						event.getBlockPlaced().setTypeId(blockType);
+						event.getBlockPlaced().setTypeIdAndData(blockType,
+								(byte) 1, false);
+						event.getBlockPlaced().getRelative(BlockFace.UP)
+								.setTypeIdAndData(blockType, (byte) 1, false);
+					} else if ((yaw >= -315 && yaw < -225)
+							|| (yaw >= 45 && yaw < 135)) {
+						event.getBlockPlaced().setTypeId(blockType);
+						event.getBlockPlaced().setTypeIdAndData(blockType,
+								(byte) 2, false);
+						event.getBlockPlaced().getRelative(BlockFace.UP)
+								.setTypeIdAndData(blockType, (byte) 2, false);
+					}
 				}
 			}
 		}
 	}
 
-	@EventHandler(priority = EventPriority.NORMAL)
+	@EventHandler(priority = EventPriority.HIGH)
 	private void blockBreak(BlockBreakEvent event) {
-		int doorType = event.getBlock().getTypeId();
-		if (doorType == 64 || doorType == 71) {
-			int blockAbove = event.getBlock().getRelative(BlockFace.UP)
-					.getTypeId();
-			int blockBelow = event.getBlock().getRelative(BlockFace.DOWN)
-					.getTypeId();
-			if (blockAbove == 64 || blockAbove == 71) {
-				event.getBlock().getRelative(BlockFace.UP).setTypeId(0);
-			}
-			if (blockBelow == 64 || blockBelow == 71) {
-				event.getBlock().getRelative(BlockFace.DOWN).setTypeId(0);
+		if (!event.isCancelled()) {
+			if (doors) {
+				int doorType = event.getBlock().getTypeId();
+				if (doorType == 64 || doorType == 71) {
+					int blockAbove = event.getBlock().getRelative(BlockFace.UP)
+							.getTypeId();
+					int blockBelow = event.getBlock()
+							.getRelative(BlockFace.DOWN).getTypeId();
+					if (blockAbove == 64 || blockAbove == 71) {
+						event.getBlock().getRelative(BlockFace.UP).setTypeId(0);
+					}
+					if (blockBelow == 64 || blockBelow == 71) {
+						event.getBlock().getRelative(BlockFace.DOWN)
+								.setTypeId(0);
+					}
+				}
 			}
 		}
 	}
